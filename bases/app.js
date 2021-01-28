6355fb8b813ff271d1faa0a96908c2df465fb64c
David Barcenas <daveepro@outlook.com>
2021-01-27 21:49:05 -0600

    Take first steps with yargs

--- bases/app.js
+++ bases/app.js
@@ -1,13 +1,26 @@
+const argv = require('yargs')
+  .command('list', 'Print the multiplication table on the console', {
+    base: {
+      demandOption: true,
+      alias: 'b',
+    },
+    limit: {
+      alias: 'l',
+      default: 10,
+    },
+  })
+  .help().argv;
 const { createFile } = require('./multiply/table');
 
-let argv = process.argv;
-let param = argv[2];
-let base = param.split('=')[1];
+let processArgv = process.argv;
+console.log(argv.base, argv.limit);
+// let param = argv[2];
+// let base = param.split('=')[1];
 
-createFile(base)
-  .then((resp) => {
-    console.log(resp);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// createFile(base)
+//   .then((resp) => {
+//     console.log(resp);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
