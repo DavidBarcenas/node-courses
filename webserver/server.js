e6d8d966427689de90bad090a391fbb53029747d
David Barcenas <daveepro@outlook.com>
2021-02-08 20:46:43 -0600

    webserver: Install hbs and bootstrap

--- webserver/server.js
+++ webserver/server.js
@@ -3,14 +3,14 @@ const app = express();
 
 app
   .use(express.static(__dirname + '/public'))
-  //   .get('/', (req, res) => {
-  //     let response = {
-  //       name: 'Dave',
-  //       age: 27,
-  //       url: req.url,
-  //     };
-  //     res.send(response);
-  //   })
+  // hbs engine
+  .set('view engine', 'hbs')
+  .get('/', (req, res) => {
+    res.render('home', {
+      name: 'Daveepro',
+      year: new Date().getFullYear(),
+    });
+  })
   .listen(8080, () => {
     console.log('list port 8080');
   });
