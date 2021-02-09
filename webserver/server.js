b0a67017ba8ce923c408c6aea05d56b660e46f49
David Barcenas <daveepro@outlook.com>
2021-02-08 19:41:07 -0600

    webserver: Serve static content

--- webserver/server.js
+++ webserver/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 
 app
-  .get('/', (req, res) => {
-    let response = {
-      name: 'Dave',
-      age: 27,
-      url: req.url,
-    };
-    res.send(response);
-  })
+  .use(express.static(__dirname + '/public'))
+  //   .get('/', (req, res) => {
+  //     let response = {
+  //       name: 'Dave',
+  //       age: 27,
+  //       url: req.url,
+  //     };
+  //     res.send(response);
+  //   })
   .listen(8080, () => {
     console.log('list port 8080');
   });
