82fdc1d7ff4cf1143bc7d264f83ef2e073b5c61c
David Barcenas <daveepro@outlook.com>
2021-02-08 21:21:25 -0600

    webserver: Use partials and helpers hbs

--- webserver/server.js
+++ webserver/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('capitalize', (text) => {
+  let upper = text.substring(0, 1).toUpperCase();
+  let rest = text.substring(1).toLowerCase();
+  return upper + rest;
+});
 app
   .use(express.static(__dirname + '/public'))
   // hbs engine
@@ -8,8 +18,10 @@ app
   .get('/', (req, res) => {
     res.render('home', {
       name: 'Daveepro',
-      year: new Date().getFullYear(),
     });
+  })
+  .get('/about', (req, res) => {
+    res.render('about');
   })
   .listen(8080, () => {
     console.log('list port 8080');
